ce6973f78cf8c1806103f3bd8908e65a95595507
Haley Schafer <hschafer@umich.edu>
2018-06-22 11:16:53 +0000

    secret key fixes

--- groupblog/settings/prod.py
+++ groupblog/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 import dj_database_url
 
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DATABASES = {
  'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
